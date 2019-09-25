1d433feed05258540604a43001ba12383224cd8d
Antonio Galli <antonio-94_@live.it>
2019-09-25 13:11:25 +0200

    Upload documento in form e codice commentato in app.py per la gestione del file

--- Example/app.py
+++ Example/app.py
@@ -1,8 +1,48 @@
-from flask import Flask, render_template, json, request
+import os
+import urllib.request
+from flask import Flask, flash, render_template, json, request, redirect
 from Model.gestore import *
 #from werkzeug import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
+
+
+
+UPLOAD_FOLDER = '/Users/antonio/Downloads'
+
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+'''
+@app.route('/', methods=['POST'])
+def upload_file():
+	if request.method == 'POST':
+        # check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		if file.filename == '':
+			flash('No file selected for uploading')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			flash('File successfully uploaded')
+			return redirect('/')
+		else:
+			flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+			return redirect(request.url)
+'''
 
 @app.route('/')
 def main():
